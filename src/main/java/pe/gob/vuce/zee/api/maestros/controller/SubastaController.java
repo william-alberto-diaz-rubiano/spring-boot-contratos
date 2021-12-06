b0f3cab5fc161af860de8f4d4f0d10a135a0ba8d
Warlen <wvl0213@hotmail.com>
2021-12-06 09:26:23 -0500

    Cambios contratos

--- src/main/java/pe/gob/vuce/zee/api/maestros/controller/SubastaController.java
+++ src/main/java/pe/gob/vuce/zee/api/maestros/controller/SubastaController.java
@@ -1,7 +1,10 @@
 package pe.gob.vuce.zee.api.maestros.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import pe.gob.vuce.zee.api.maestros.base.Constantes;
+import pe.gob.vuce.zee.api.maestros.dto.ResponseDTO;
 import pe.gob.vuce.zee.api.maestros.dto.SubastaDTO;
 import pe.gob.vuce.zee.api.maestros.service.SubastaService;
 
@@ -16,7 +19,10 @@ public class SubastaController {
     }
 
     @PutMapping()
-    public SubastaDTO saveSubasta(@RequestBody @Valid SubastaDTO subastaDTO){
-        return subastaService.saveSubasta(subastaDTO);
+    public ResponseEntity<ResponseDTO<?>> saveSubasta(@RequestBody @Valid SubastaDTO subastaDTO){
+        ResponseDTO<?> response;
+        SubastaDTO subastaDTO1 = subastaService.saveSubasta(subastaDTO);
+        response = new ResponseDTO<>(Constantes.NO_ERROR, subastaDTO1);
+        return ResponseEntity.ok(response);
     }
 }
